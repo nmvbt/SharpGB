ede020d385909aec1ac5949d4a620922237e5d1f
Nikita Rykov <nikita.kest2011@yandex.ru>
2023-01-29 22:24:31 +0300

    Condition if/else

---
+++ Example005_Condition_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+    Console.WriteLine("Hey hey, its Maria over here!");
+} 
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}

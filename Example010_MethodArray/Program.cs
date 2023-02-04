6f64b8f6e8c57baf0c8d496e625212be43ff742f
Nikita Rykov <nikita.kest2011@yandex.ru>
2023-02-04 22:28:56 +0300

    new lection

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {1, 22, 13, 210, 34, 25, 63, 67, 28 ,90 ,210};
+
+int n = array.Length;
+int find = 210;
+int index = 0;
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
